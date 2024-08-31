3a0e8b29e05cfbdbc112bb337ea7a4537c6a21af
user.email <devendrachhipi456@gmail.com>
2024-08-31 15:51:25 +0530

    signing issue resolved

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors'; // Import the CORS middleware
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+
 dotenv.config();
 
 mongoose
@@ -17,12 +19,14 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.json());
+// Enable CORS for all routes
+app.use(cors());
 
+app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
@@ -33,12 +37,11 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
