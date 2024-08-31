8bbc479249518a349906612e688523cf5bd114b2
user.email <devendrachhipi456@gmail.com>
2024-08-31 16:12:42 +0530

    sign issue

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,9 @@
-// Firebase initialization
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-estate-21774.firebaseapp.com",
@@ -12,6 +14,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-auth.useDeviceLanguage(); // Ensure proper language settings
+export const app = initializeApp(firebaseConfig);
