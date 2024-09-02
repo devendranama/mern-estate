9e2ed0f92ab30573707a8f74a24db2aa28082cda
user.email <devendrachhipi456@gmail.com>
2024-09-02 20:40:20 +0530

    backend index rollback

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Import the CORS middleware
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
 dotenv.config();
 
 mongoose
@@ -19,37 +17,29 @@ mongoose
     console.log(err);
   });
 
-// const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
 const app = express();
-app.use(cors()); // Use the CORS middleware
-// Middleware to set COOP and COEP headers
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin'); // or 'same-origin-allow-popups' if using popups
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp'); // required for some cross-origin requests
-  next();
-});
-
 
 app.use(express.json());
+
 app.use(cookieParser());
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(3000, () => {
   console.log('Server is running on port 3000!');
-  // res.json({ message: 'Server is running on port 3000!' });
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// Error handling middleware
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
