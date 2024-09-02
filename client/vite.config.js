0fb24effc2365d743a5d961037d9f9f2f712cf8c
user.email <devendrachhipi456@gmail.com>
2024-09-02 21:05:43 +0530

    testing backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default {
   server: {
     proxy: {
       '/api': {
-        target: 'https://mern-estatebackend-devendras-projects-b81dd99f.vercel.app/', // Ensure the target uses HTTPS
+        target: 'https://mern-estatebackend-devendras-projects-b81dd99f.vercel.app', // Ensure the target uses HTTPS
         changeOrigin: true, // Needed to handle the origin change
         secure: true, // If using self-signed certificates, set this to false
       },
