51d32e84046a305fa86af8e69464e7ba3498de5a
user.email <devendrachhipi456@gmail.com>
2024-09-03 12:33:16 +0530

    cors resolved

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default {
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://mern-estatebackend-devendras-projects-b81dd99f.vercel.app', // Ensure the target uses HTTPS
-  //       changeOrigin: true, // Needed to handle the origin change
-  //       secure: true, // If using self-signed certificates, set this to false
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mern-estatebackend-devendras-projects-b81dd99f.vercel.app', // Ensure the target uses HTTPS
+        changeOrigin: true, // Needed to handle the origin change
+        secure: true, // If using self-signed certificates, set this to false
+      },
+    },
+  },
   plugins: [react()],
 };
 
